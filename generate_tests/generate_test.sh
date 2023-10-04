952779e9c78880798f08d2e4c6b29dae7de3a559
ddzingel <81826555+ddzingel@users.noreply.github.com>
2023-10-04 10:57:42 -0400

    Switch from sys.argv to argparse

--- generate_tests/generate_test.sh
+++ generate_tests/generate_test.sh
@@ -3,4 +3,4 @@ cd ..
 mkdir $1
 cp generate_tests/input_file.json $1/
 cd $1
-python /home/davidjay.dzingeleski/config_tests/generate_tests/MakeTestPipeline.py input_file.json $1
+python /home/davidjay.dzingeleski/config_tests/generate_tests/MakeTestPipeline.py --file "input_file.json" --name "$1"
