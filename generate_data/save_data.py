b2abba9c147e315cce4d40092d16652e515a6c09
ddzingel <81826555+ddzingel@users.noreply.github.com>
2023-10-03 23:12:09 -0400

    Added comments

--- generate_data/save_data.py
+++ generate_data/save_data.py
@@ -35,14 +35,15 @@ def parse():
     args = parser.parse_args()
     return args
 
+#load variables from input arguments
 args = parse()
-
 M = args.M
 q = args.q
 spin_1 = args.spin1
 spin_2 = args.spin2
 snr = args.snr
 dist_mpc = args.distance
+#update injection file with new values
 with open("injection_kws.json","r") as file_object:
     json_data = json.load(file_object)
 json_data["mass_1"] = q*M/(1+q)
@@ -52,13 +53,14 @@ json_data["spin_2z"] = spin_2
 json_data["luminosity_distance"] = dist_mpc
 with open("injection_kws.json","w") as file_object:
     json.dump(json_data,file_object,indent=2)
+#update config file with path to injection file
 config = configparser.ConfigParser()
 config.read("test_injection.ini")
-config['prior']['M_min'] = str(M/2)
-config['prior']['M_max'] = str(2*M)
 config['injection']['path'] = "injection_kws.json"
 with open('test_injection.ini', 'w') as configfile:
   config.write(configfile)
+#create fit object from config file with injected data in it
 fit = ringdown.fit.Fit.from_config("test_injection.ini")
+#save injection data for each ifo to csv files
 fit.data["H1"].to_csv("H1_{}_{}_{}_{}_{}.csv".format(int(M),int(q),int(10*spin_1),int(10*spin_2),int(snr)))
 fit.data["L1"].to_csv("L1_{}_{}_{}_{}_{}.csv".format(int(M),int(q),int(10*spin_1),int(10*spin_2),int(snr)))
