04a0f5c7deea60d66693a1bd6dcff72988aac4c9
ddzingel <81826555+ddzingel@users.noreply.github.com>
2023-10-03 23:17:33 -0400

    Added comments

--- generate_tests/ringdownfit.py
+++ generate_tests/ringdownfit.py
@@ -35,8 +35,8 @@ def parse():
     args = parser.parse_args()
     return args
 
+#read arguments from input
 args = parse()
-
 t_sun = 5*10**-6
 M = args.M
 q = args.q
@@ -45,15 +45,23 @@ spin_2 = args.spin2
 snr = args.snr
 time = args.time
 time_index = (time - 1126259463.413)/(M*t_sun)
+#update config file
 config = configparser.ConfigParser()
 config.read("test_injection.ini")
+#prior for each mass is M/2 to 2M
 config['prior']['M_min'] = str(M/2)
 config['prior']['M_max'] = str(2*M)
+#update analysis start time
 config['target']['t0'] = str(time)
+#set path to data file
 config['data']['path'] = "/home/davidjay.dzingeleski/config_tests/data/Results/{{ifo}}_{}_{}_{}_{}_{}.csv".format(int(M),int(q),int(10*spin_1),int(10*spin_2),int(snr))
+#write to config file
 with open('test_injection.ini', 'w') as configfile:
   config.write(configfile)
+#generate fit from config file, data saved using generate_data is already conditioned, so don't need to condition
 fit = ringdown.fit.Fit.from_config("test_injection.ini",no_cond=True)
+#run the fit
 fit.run()
+#save the results to a json file
 dataframe = fit.result.to_dataframe()
 dataframe.to_json('results{}_{}_{}_{}_{}_{}.json'.format(int(M),int(q),int(10*spin_1),int(10*spin_2),int(snr),int(time_index)))
