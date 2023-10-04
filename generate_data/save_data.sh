2a915cd3e3797bb528042b64ee99e74e734d6470
ddzingel <81826555+ddzingel@users.noreply.github.com>
2023-10-04 11:00:36 -0400

    Switch from sys.argv to argparse

--- generate_data/save_data.sh
+++ generate_data/save_data.sh
@@ -3,5 +3,5 @@ cd ..
 mkdir $1
 cp generate_data/input_file.json $1/
 cd $1
-python /home/davidjay.dzingeleski/config_tests/generate_data/Compute_SNRs.py input_file.json
-python /home/davidjay.dzingeleski/config_tests/generate_data/MakeTestPipeline.py input_file.json $1
+python /home/davidjay.dzingeleski/config_tests/generate_data/Compute_SNRs.py --file "input_file.json"
+python /home/davidjay.dzingeleski/config_tests/generate_data/MakeTestPipeline.py --file "input_file.json" --name "$1"
