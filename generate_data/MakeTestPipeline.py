00fcdc262b14e235c1e04ef25651170687c52a6f
ddzingel <81826555+ddzingel@users.noreply.github.com>
2023-10-03 23:08:10 -0400

    Added comments

--- generate_data/MakeTestPipeline.py
+++ generate_data/MakeTestPipeline.py
@@ -5,20 +5,23 @@ import sys
 import numpy as np
 import json
 
-
+#set user name and accounting tag for submission file
 ligo_user_name = "davidjay.dzingeleski"
 ligo_accounting = "ligo.dev.o4.cbc.pe.bilby"
 
+#set up directories for result files and log/error files
 pipeline_dir = os.path.join(os.getcwd(), "Results")
 os.makedirs(pipeline_dir)
 logdir = os.path.join(pipeline_dir, "logs")
 os.makedirs(logdir)
 
+#set up dag
 dag = pipeline.CondorDAG(
     log=os.path.join(pipeline_dir, "dag_{}.log".format(str(sys.argv[2])))
 )
 dag.set_dag_file(os.path.join(pipeline_dir, "dag_{}".format(str(sys.argv[2]))))
 
+#set up job with additional commands
 worker_job = condorutils.standard_job_constructor(
     "save_data.py",
     str(sys.argv[2]),
@@ -30,8 +33,10 @@ worker_job = condorutils.standard_job_constructor(
     logdir=logdir,
     extra_cmds = [["transfer_input_files","/home/davidjay.dzingeleski/config_tests/generate_data/injection_kws.json, /home/davidjay.dzingeleski/config_tests/generate_data/test_injection.ini"],["transfer_executable",True],["should_transfer_files","YES"],["getenv",True]]
 )
+#set executable for job
 worker_job.set_executable("/home/davidjay.dzingeleski/config_tests/generate_data/save_data.py")
 
+#read parameter values from input file
 file = str(sys.argv[1])
 with open(file,"r") as readfile:
     data = json.load(readfile)
@@ -39,19 +44,23 @@ mass = np.array(data["mass"])
 mass_ratio = np.array(data["mass_ratio"])
 spin_1 = np.array(data["spin_1"])
 spin_2 = np.array(data["spin_2"])
+#read SNRs at 1 Mpc and reshape to be a multidimensional array
 SNRs_at_1 = np.reshape(np.array(data["SNRs_at_1"]),(len(mass),len(mass_ratio),len(spin_1),len(spin_2)))
 SNR = np.array(data["SNR"])
+#set up list to store jobs with each parameter combination
 worker_nodes = []
 for i in range(len(mass)):
     for j in range(len(mass_ratio)):
         for k in range(len(spin_1)):
             for l in range(len(spin_2)):
                 for m in range(len(SNR)):
+                    #create a job with parameter combinations
                     worker_node = condorutils.standard_node_constructor(
                         worker_job,
                         dag,
                         macros=[("M", mass[i]), ("q", mass_ratio[j]), ("spin1", spin_1[k]), ("spin2", spin_2[l]), ("snr", SNR[m]), ("distance", SNRs_at_1[i][j][k][l]/SNR[m])],
                         )
+                    #add job to list
                     worker_nodes += [worker_node]
 
 # Write sub files and dag
